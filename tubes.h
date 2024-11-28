5a0e480660949294093e0bbd5c042c9ba33c9d37
fazarirazkaa <fazari.razka50@gmail.com>
2024-11-28 10:17:16 +0700

    chore: add data structure

--- tubes.h
+++ tubes.h
@@ -4,5 +4,28 @@
 #include <iostream>
 using namespace std;
 
+struct karyawan {
+    int ID_karyawan;
+    string nama_karyawan;
+    string alamat_karyawan;
+    int no_telp_karyawan;
+    string email_karyawan;
+};
+
+struct projek {
+    int ID_projek;
+    string nama_projek;
+    string jenis_projek;
+    int durasi_projek;
+    int anggaran_projek;
+};
+
+void insertFirstKaryawan();
+void showAllKaryawan();
+void deleteKaryawanDanProjek();
+void searchKaryawan();
+void insertLastProjek();
+
+
 
 #endif // TUBES_H_INCLUDED
